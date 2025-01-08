8fd110fa277bcb1936f7e5b2b11baeeebf35e8fa
yangwenqin <yangwq@friendcom.com>
2025-01-08 17:34:28 +0800

    添加直接插入一天股票数据到数据库的方式,方便更新

--- database/test/save_direct_to_db.py
+++ database/test/save_direct_to_db.py
@@ -4,20 +4,25 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# 读取CSV文件
-csv_file_path = '../stock_csv/tushare_daily_20250107085416.csv'  # 替换为您的CSV文件路径
-df = pd.read_csv(csv_file_path)
-
-# 由于CSV文件没有stock_name列，我们需要添加这一列
-df['stock_name'] = '永辉超市'
-df['trade_date'] = '2025-01-07'
-df['vol'] = '5690287'
-df['open'] = '5.85'
-df['high'] = '6.05'
-df['low'] = '5.69'
-df['close'] = '5.97'
-df['pct_chg'] = '0.1678'
+# 创建一个包含一行数据的 DataFrame
+data = {
+    'stock_name': ['永辉超市'],
+    'trade_date': ['2025-01-08'],
+    'open': [5.95],
+    'high': [6.09],
+    'low': [5.74],
+    'close': [5.94],
+    'pct_chg': [-0.5025],
+    'vol': [5049431.55]
+}
+df = pd.DataFrame(data)
 
 # 创建数据库连接引擎
 # 请根据实际情况替换用户名、密码、主机和数据库名
 engine = create_engine('mysql://root:@127.0.0.1:3306/stock?charset=utf8&use_unicode=1')
+
+try:
+    df.to_sql('stock_daily', con=engine, if_exists='append', index=False)
+    print("数据已成功插入到stock_daily表中")
+except Exception as e:
+    print(f"发生错误: {e}")
